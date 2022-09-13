d87d3f7adb8fc98caa07d647020282fada8e1e92
Kevin <kevin.castro.chavez0@gmail.com>
2022-09-13 09:20:32 -0600

    got the current date for the assignment

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿string fullName = "Kevin";
+﻿using System;
+string fullName = "Kevin";
 string location = "Rexburg";
 
 Console.WriteLine($"My name is {fullName}. I'm from {location}");
+
+string currentDate = DateTime.Now.ToString("yyyy-MM-dd");
+Console.WriteLine(currentDate);
