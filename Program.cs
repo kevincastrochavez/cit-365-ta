790f5350e83b5bf0798a09898160dc55b2dc83c2
Kevin <kevin.castro.chavez0@gmail.com>
2022-09-13 09:31:22 -0600

    got days until christmas for this year

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Runtime.CompilerServices;
+using System;
 string fullName = "Kevin";
 string location = "Rexburg";
 
@@ -6,3 +7,6 @@ Console.WriteLine($"My name is {fullName}. I'm from {location}");
 
 string currentDate = DateTime.Now.ToString("yyyy-MM-dd");
 Console.WriteLine(currentDate);
+
+int date = DateTime.Now.DayOfYear;
+Console.WriteLine($"The number of days until Christmas is {360 - date}");
